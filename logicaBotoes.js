fe3c6ab9c0d840c5d94d99c33928f25712940920
Roni Cleber <ronycleber_pm@hotmail.com>
2020-12-19 15:53:53 -0300

    Comite inicial

---
+++ logicaBotoes.js
@@ -0,0 +1,55 @@
+function btn0(){
+	botoes[0].innerHTML = botoes[0].innerHTML === 'X' ? '0':'X'
+	botoes[1].innerHTML = botoes[1].innerHTML === 'X' ? '0':'X'
+	botoes[3].innerHTML = botoes[3].innerHTML === 'X' ? '0':'X'
+}
+
+function btn1(){
+	botoes[0].innerHTML = botoes[0].innerHTML === 'X' ? '0':'X'
+	botoes[1].innerHTML = botoes[1].innerHTML === 'X' ? '0':'X'
+	botoes[2].innerHTML = botoes[2].innerHTML === 'X' ? '0':'X'
+}
+
+function btn2(){
+	botoes[1].innerHTML = botoes[1].innerHTML === 'X' ? '0':'X'
+	botoes[2].innerHTML = botoes[2].innerHTML === 'X' ? '0':'X'
+	botoes[5].innerHTML = botoes[5].innerHTML === 'X' ? '0':'X'
+}
+
+function btn3(){
+	botoes[0].innerHTML = botoes[0].innerHTML === 'X' ? '0':'X'
+	botoes[3].innerHTML = botoes[3].innerHTML === 'X' ? '0':'X'
+	botoes[6].innerHTML = botoes[6].innerHTML === 'X' ? '0':'X'
+}
+
+function btn4(){
+	botoes[1].innerHTML = botoes[1].innerHTML === 'X' ? '0':'X'
+	botoes[3].innerHTML = botoes[3].innerHTML === 'X' ? '0':'X'
+	botoes[4].innerHTML = botoes[4].innerHTML === 'X' ? '0':'X'
+	botoes[5].innerHTML = botoes[5].innerHTML === 'X' ? '0':'X'
+	botoes[7].innerHTML = botoes[7].innerHTML === 'X' ? '0':'X'
+}
+
+function btn5(){
+	botoes[2].innerHTML = botoes[2].innerHTML === 'X' ? '0':'X'
+	botoes[5].innerHTML = botoes[5].innerHTML === 'X' ? '0':'X'
+	botoes[8].innerHTML = botoes[8].innerHTML === 'X' ? '0':'X'
+}
+
+function btn6(){
+	botoes[3].innerHTML = botoes[3].innerHTML === 'X' ? '0':'X'
+	botoes[6].innerHTML = botoes[6].innerHTML === 'X' ? '0':'X'
+	botoes[7].innerHTML = botoes[7].innerHTML === 'X' ? '0':'X'
+}
+
+function btn7(){
+	botoes[6].innerHTML = botoes[6].innerHTML === 'X' ? '0':'X'
+	botoes[7].innerHTML = botoes[7].innerHTML === 'X' ? '0':'X'
+	botoes[8].innerHTML = botoes[8].innerHTML === 'X' ? '0':'X'
+}
+
+function btn8(){
+	botoes[5].innerHTML = botoes[5].innerHTML === 'X' ? '0':'X'
+	botoes[7].innerHTML = botoes[7].innerHTML === 'X' ? '0':'X'
+	botoes[8].innerHTML = botoes[8].innerHTML === 'X' ? '0':'X'
+}
